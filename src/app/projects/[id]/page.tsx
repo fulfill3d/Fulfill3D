cc182ac89b4246d8190bf37edf94a3430a704ca8
Abdurrahman Gazi Yavuz <sniper.erratic.0w@icloud.com>
2024-09-29 15:42:24 -0700

    loading feedback on routing, not found feedback on 404 pages

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,35 +1,50 @@
 "use client";
 
-import {ProjectWiki} from "@/models/project/wiki/project-wiki";
+import { ProjectWiki } from "@/models/project/wiki/project-wiki";
 import ProjectWikiComponent from "@/components/project/wiki/project-wiki-component";
-import {useEffect, useState} from "react";
-import {projectList} from "@/mock/project/data";
+import { useEffect, useState } from "react";
+import { projectList } from "@/mock/project/data";
+import Loading from "@/app/loading";
+import NotFound from "@/components/common/not-found";
 
-// Mock function to fetch post based on id (replace this with actual API call)
-const fetchWikiById = (uid: string): ProjectWiki | null => {
-    const data = projectList.find((product) => product.uuid === uid);
-    if (data) {
-        return ProjectWiki.fromJson(data.wiki);
-    }
-    return null;
+// Mock async function to fetch wiki based on id (simulate async behavior)
+const fetchWikiById = async (uid: string): Promise<ProjectWiki | null> => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const data = projectList.find((product) => product.uuid === uid);
+            if (data) {
+                resolve(ProjectWiki.fromJson(data.wiki));
+            } else {
+                resolve(null);
+            }
+        }, 600); // Simulate a 1.5-second delay
+    });
 };
 
 export default function Wiki({ params }: { params: { id: string } }) {
     const { id } = params; // Get the dynamic id from the route
     const [projectWiki, setProjectWiki] = useState<ProjectWiki | null>(null);
+    const [loading, setLoading] = useState(true); // Track loading state
 
     useEffect(() => {
-        if (id) {
-            const fetchedPost = fetchWikiById(id); // Fetch the post by id
-            setProjectWiki(fetchedPost);
-        }
+        const fetchWiki = async () => {
+            if (id) {
+                setLoading(true);
+                const fetchedWiki = await fetchWikiById(id); // Fetch the wiki by id asynchronously
+                setProjectWiki(fetchedWiki);
+                setLoading(false); // Stop loading after the fetch
+            }
+        };
+        fetchWiki();
     }, [id]);
 
-    if (!projectWiki) {
-        return <div>Loading...</div>; // Show a loading state while fetching the post
+    if (loading) {
+        return <Loading />;
     }
 
-    return(
-        <ProjectWikiComponent wiki={projectWiki}/>
-    );
+    if (!projectWiki) {
+        return <NotFound/>;
+    }
+
+    return <ProjectWikiComponent wiki={projectWiki} />;
 }
