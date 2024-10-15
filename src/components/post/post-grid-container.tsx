c2117a46b6ff2be7c0bb8079f9b871abc72a9c0f
Abdurrahman Gazi Yavuz <sniper.erratic.0w@icloud.com>
2024-10-15 15:34:07 -0700

    reverse posts in grid container

--- src/components/post/post-grid-container.tsx
+++ src/components/post/post-grid-container.tsx
@@ -16,7 +16,7 @@ const PostGridContainer: React.FC<PostGridContainerProps> = () => {
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {posts.map((post) => (<PostMetadataCard key={post.id} post={post} />))}
+            {posts.map((post) => (<PostMetadataCard key={post.id} post={post} />)).reverse()}
         </div>
     );
 };
